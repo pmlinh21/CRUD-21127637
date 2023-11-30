ad60d37a2cce2ab74fff2de933da5ba72e59c547
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2023-11-30 10:56:55 +0700

    deploy

---
+++ html/assets/js/user-management.js
@@ -0,0 +1,71 @@
+async function deleteUser(id){
+  let res = await fetch(`/users/${id}`, {
+    method: "DELETE"
+  })
+  location.reload()
+}
+
+
+function showEditUserModal(btn){
+  console.log(btn.dataset)
+  document.querySelector("#id").value = btn.dataset.id;
+  document.querySelector("#usernameEdit").value = btn.dataset.username;
+  document.querySelector("#firstNameEdit").value = btn.dataset.firstName;
+  document.querySelector("#lastNameEdit").value = btn.dataset.lastName;
+  document.querySelector("#mobileEdit").value = btn.dataset.mobile;
+  document.querySelector("#isAdminEdit").checked = btn.dataset.isAdmin == "true"
+  ? true : false ;
+  document.querySelector("#editUserForm").action =`/users/${btn.dataset.id}`;
+}
+
+async function editUser(e){
+  e.preventDefault();
+
+  const formData = new FormData(document.querySelector("#editUserForm"))
+  const data = Object.fromEntries(formData.entries())
+  console.log(JSON.stringify(data))
+
+  let res = await fetch("/users",{
+    method: "PUT",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+
+  location.reload()
+}
+
+document
+  .querySelector("#editUserModal")
+  .addEventListener("shown.bs.modal", () => {
+    document.querySelector("#firstNameEdit").focus();
+  });
+
+document
+  .querySelector("#addUserModal")
+  .addEventListener("shown.bs.modal", () => {
+    document.querySelector("#firstName").focus();
+  });
+
+document.querySelectorAll(".delete-btn").forEach((btnConfirm) => {
+  btnConfirm.addEventListener("click", (e) => {
+    const options = {
+      title: "Are you sure?",
+      type: "danger",
+      btnOkText: "Yes",
+      btnCancelText: "No",
+      onConfirm: () => {
+        deleteUser(e.target.dataset.id);
+      },
+      onCancel: () => {
+        console.log("Cancel");
+      },
+    };
+    const {
+      el,
+      content,
+      options: confirmedOptions,
+    } = bs5dialog.confirm("Do you really want to delete this user?", options);
+  });
+});
